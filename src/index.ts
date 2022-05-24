4bc45358c369dbb9c0c1829a6af39898142a2794
Renan Zelaya <renan_zelaya@hotmail.com>
2022-05-23 23:06:54 -0600

    Add default export

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,7 @@ export * from './CloudWatch';
 export * from './EcsEc2';
 export * from './EcsTaskRole';
 export * from './Naming';
+export * from './Rds';
+export * from './RdsDatabase';
+export * from './SecurityGroup';
 export * from './StaticWebsite';
